63100791566c25800091c76460c5eb62a61e8952
PraptiNayak25 <prapticnayak@gmail.com>
2025-06-10 09:47:43 +0530

    save

---
+++ Array.java
@@ -0,0 +1,28 @@
+public class Array{
+    
+    public static void main(String[] args){
+        int [] arr={1,2,3,4};
+        int sum=0;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+
+        for(int values:arr){
+            sum+=values;
+            if(values>max){
+                max=values;
+
+            }
+
+            if(values<min){
+                min=values;
+
+            }
+
+        }
+        
+
+        System.out.println("Sum :"+sum);
+        System.out.println("MAx :"+max);
+        System.out.println("Min :"+min);
+    }
+}
